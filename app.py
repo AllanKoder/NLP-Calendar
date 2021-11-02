6c3129d236c33726314ced14b6b20adae0b38565
Allan Kong <allankong176@gmail.com>
2021-11-02 12:47:08 -0600

    first push

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route('/')
+def Home():
+    return render_template("home.html")
+
+@app.route('/',methods=['POST'])
+def multiply():
+    if request.method == "POST":
+        number1 = request.form['text1']
+        try:
+            return f"<h1>product is: {float(number1)}</h1>"
+        except:
+            return ""
+'''
+@app.route("/predict", methods=['POST'])
+def prediction():
+'''
+if __name__ == "__main__":
+    app.run(debug=True)
+
